914aab08ff95c4ed62eec640c755b3a58d6e8f76
dianedouglas <diane.douglas1@gmail.com>
2014-08-12 22:37:19 -0700

    Create add_cd method to add a cd object to the genre's library of cds, and a library method to return an array of the added cds.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,18 @@
+require 'cd'
+
 class Genre
 
-  attr_accessor :name
+  attr_accessor :name, :library
 
   def initialize(attributes)
     @name = attributes['name']
+    @library = attributes['library']
+    if @library == nil
+      @library = []
+    end
+  end
+
+  def add_cd(cd)
+    @library << cd
   end
 end
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,6 +7,7 @@ describe 'Genre' do
 
   before do
     @test_genre = Genre.new({'name' => 'Black Metal'})
+    @test_cd = Cd.new({:album_name => 'Konigsforst', :artist_name => 'Gas'})
   end
 
   describe '#initialize' do
@@ -22,6 +23,14 @@ describe 'Genre' do
       expect(@test_genre.name).to eq 'Black Metal'
       @test_genre.name = 'Techno'
       expect(@test_genre.name).to eq 'Techno'
+    end
+  end
+
+  describe '#add_cd' do
+
+    it 'adds a cd object to the library for this genre.' do
+      @test_genre.add_cd(@test_cd)
+      expect(@test_genre.library).to eq [@test_cd]
     end
   end
 end
