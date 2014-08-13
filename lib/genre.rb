bfb4d858b2f84f9b4d173388a754b26b44da81c8
dianedouglas <diane.douglas1@gmail.com>
2014-08-12 22:23:55 -0700

    Add initialize and read/write name methods for Genre class.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,7 @@
 class Genre
 
+  attr_accessor :name
+
   def initialize(attributes)
     @name = attributes['name']
   end
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -9,7 +9,19 @@ describe 'Genre' do
     @test_genre = Genre.new({'name' => 'Black Metal'})
   end
 
-  it 'is initialized with a name.' do
-    expect(@test_genre).to be_an_instance_of Genre
+  describe '#initialize' do
+
+    it 'is initialized with a name.' do
+      expect(@test_genre).to be_an_instance_of Genre
+    end
+  end
+
+  describe '#name' do
+
+    it 'returns the genre name and allows you to write into it.' do
+      expect(@test_genre.name).to eq 'Black Metal'
+      @test_genre.name = 'Techno'
+      expect(@test_genre.name).to eq 'Techno'
+    end
   end
 end
